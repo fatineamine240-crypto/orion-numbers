8fc7b867e4f1258fed72cf225429f11e47255f37
edriso <edriso.dev@gmail.com>
2023-12-21 19:48:13 +0200

    update padding and layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,13 @@ import logo from '../assets/logo.png';
 export default function Header() {
   return (
     <header className="bg-white dark:bg-gray-900">
-      <div className="flex items-center justify-center px-6 py-3 md:py-4">
+      <div className="flex items-center justify-center">
         <Link to="/" className="flex items-center gap-2">
-          <p className="text-xs">Number</p>
+          <span>Number</span>
           <img className="w-auto h-[4rem]" src={logo} alt="Logo" />
-          <p className="text-xs">Ninjas</p>
+          <span>Ninjas</span>
         </Link>
       </div>
-
-      <hr className="border-gray-200 dark:border-gray-700" />
     </header>
   );
 }
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -5,7 +5,13 @@ export default function AppLayout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <hr className="border-gray-200 dark:border-gray-700 my-3 md:my-5 -mx-4 md:-mx-10 xl:-mx-14" />
+
+      <main className="flex-1 max-w-full w-[50rem] mx-auto pt-1">
+        <Outlet />
+      </main>
+
+      <hr className="border-gray-200 dark:border-gray-700 my-3 md:my-5 -mx-4 md:-mx-10 xl:-mx-14" />
       <Footer />
     </>
   );
