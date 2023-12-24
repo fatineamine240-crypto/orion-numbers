2dc2b1140aba95ad245ce089f6084c6acf16e3b9
edriso <edriso.dev@gmail.com>
2023-12-24 23:21:26 +0200

    add math functions

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,27 +1,57 @@
-export const getAnswer = (num1, op, num2) => {
-  let expectedAnswer;
+import _ from 'lodash';
+import { Order } from './constants';
 
-  switch (op) {
+export const calculateArithmeticOperation = (operand1, operator, operand2) => {
+  let result;
+
+  switch (operator) {
     case '+':
-      expectedAnswer = num1 + num2;
+      result = operand1 + operand2;
       break;
     case '-':
-      expectedAnswer = num1 - num2;
+      result = operand1 - operand2;
       break;
     case '*':
     case 'x':
-      expectedAnswer = num1 * num2;
+      result = operand1 * operand2;
       break;
     case '/':
-      expectedAnswer = num1 / num2;
+      result = operand1 / operand2;
       break;
     default:
-      throw new Error(`Invalid operator: ${op}`);
+      throw new Error(`Invalid operator: ${operator}`);
   }
 
-  return expectedAnswer;
+  return result;
 };
 
-export const checkAnswer = (num1, op, num2, answer) => {
-  return answer === getAnswer(num1, op, num2);
+export const isAnswerCorrect = (operand1, operator, operand2, userAnswer) => {
+  return (
+    userAnswer === calculateArithmeticOperation(operand1, operator, operand2)
+  );
+};
+
+export const generateMultiplicationQuestions = (
+  multiplicand,
+  order = Order.ASC
+) => {
+  if (typeof multiplicand !== 'number' || multiplicand <= 0) {
+    throw new Error('Multiplicand must be a number larger than 0');
+  }
+
+  let multipliers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  if (order === Order.DESC) {
+    multipliers = multipliers.reverse();
+  } else if (order === Order.RANDOM) {
+    multipliers = _.shuffle(multipliers);
+  }
+
+  const multiplicationQuestions = multipliers.map((multiplier) => {
+    const question = `${multiplicand} x ${multiplier}`;
+    const correctAnswer = multiplicand * multiplier;
+    return { question, correctAnswer };
+  });
+
+  return multiplicationQuestions;
 };
