7a497d308b314f8dfc323435a3ce79c928d0046c
edriso <edriso.dev@gmail.com>
2023-12-26 09:52:24 +0200

    add QuestionBox logic

--- src/components/QuestionBox.jsx
+++ src/components/QuestionBox.jsx
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { Form, Input } from './Form';
+import { Button } from '.';
 
 export default function QuestionBox({
+  heading,
   currentQuestion,
   nextQuestion,
   submitAnswer,
@@ -9,39 +12,57 @@ export default function QuestionBox({
   const [answer, setAnswer] = useState('');
   const [isAnswered, setIsAnswered] = useState(false);
 
-  const handleSubmit = () => {
-    setIsAnswered(true);
-    submitAnswer(Number(answer));
-  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (answer.trim() === '') return;
 
-  const handleNext = () => {
-    setAnswer('');
-    setIsAnswered(false);
-    nextQuestion();
+    setIsAnswered(true);
+    submitAnswer(Number(answer.trim()));
+
+    setTimeout(() => {
+      setAnswer('');
+      setIsAnswered(false);
+      nextQuestion();
+    }, 500);
   };
 
   return (
-    <div>
-      <p>{currentQuestion.num1}</p>
-      <p>{currentQuestion.operation}</p>
-      <p>{currentQuestion.num2}</p>
-      <input
-        type="number"
-        name="answer"
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-      />
+    <div className="block rounded-lg bg-white text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+      <div className="capitalize border-b-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
+        {heading}
+      </div>
+      <div className="p-6">
+        <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+          {currentQuestion} =
+        </h5>
+        <Form onSubmit={handleSubmit}>
+          <div className="flex justify-center items-center gap-4">
+            <Input
+              type="number"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+            />
+          </div>
 
-      <button onClick={handleSubmit} disabled={isAnswered}>
-        Submit
-      </button>
-      <button onClick={handleNext}>next</button>
+          <hr className="border-gray-200 dark:border-gray-700 my-3 md:my-5 -mx-4 md:-mx-10 xl:-mx-14" />
+
+          <div className="mt-6">
+            <Button
+              type="submit"
+              text={!isAnswered ? 'Check Answer' : 'Answered'}
+              primary={!isAnswered}
+              disabled={isAnswered}
+            />
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
 
 QuestionBox.propTypes = {
-  currentQuestion: PropTypes.object,
+  heading: PropTypes.string,
+  currentQuestion: PropTypes.string,
   nextQuestion: PropTypes.func,
   submitAnswer: PropTypes.func,
 };
