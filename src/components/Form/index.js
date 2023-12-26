3c8f352ef65e8f5383b3fa812bea644c344e84ba
edriso <edriso.dev@gmail.com>
2023-12-26 08:32:51 +0200

    add Btn components

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,4 @@
 export { default as Form } from './Form';
 export { default as Input } from './Input';
-export { default as ButtonSubmit } from './ButtonSubmit';
-export { default as CancelButton } from './CancelButton';
 export { default as Select } from './Select';
 export { default as RadioGroup } from './RadioGroup';
