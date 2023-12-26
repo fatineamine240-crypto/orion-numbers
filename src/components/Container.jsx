5abcc37a7af6d1cfa069622beb96612b735e267c
edriso <edriso.dev@gmail.com>
2023-12-26 16:36:12 +0200

    add className prop

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-export default function Container({ children }) {
+export default function Container({ children, className }) {
   return (
-    <div className="rounded-lg border text-center shadow-md">{children}</div>
+    <div className={`rounded-lg border text-center shadow-md ${className}`}>
+      {children}
+    </div>
   );
 }
 
 Container.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
 };
