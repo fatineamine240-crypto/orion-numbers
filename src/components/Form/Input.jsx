cf1523811a4607e09966463c7e9d279a0f2bf4bf
edriso <edriso.dev@gmail.com>
2023-12-26 09:24:49 +0200

    use value instead of defaultValue

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -20,7 +20,7 @@ export default function Input({
         type={type}
         id={name}
         name={name}
-        defaultValue={value}
+        value={value}
         required={required}
         onChange={onChange}
         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
