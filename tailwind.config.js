14ea2d751b6b7e51a2c46baff96ebbfca2cd16ec
edriso <edriso.dev@gmail.com>
2023-12-26 11:06:10 +0200

    add config colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        appFont: ['Poppins', 'sans-serif'],
+      },
       colors: {
-        primary: '#f81f5e',
+        primary: 'hsl(343 94% 55% / 1)',
+        secondary: 'hsl(343 94% 50% / 1)',
       },
     },
   },
