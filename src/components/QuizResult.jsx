639a5918fb2ccb44660c796b504b5f4a4ae18fb4
edriso <edriso.dev@gmail.com>
2024-01-20 14:44:05 +0200

    update sound effects on answering

--- src/components/QuizResult.jsx
+++ src/components/QuizResult.jsx
@@ -1,9 +1,8 @@
 import PropTypes from 'prop-types';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Button, Container, Confetti } from '.';
-import perfectSoundFile from '../assets/sound/perfect.mp3';
 
 export default function QuizResult({ result, total }) {
   // eslint-disable-next-line
@@ -11,15 +10,12 @@ export default function QuizResult({ result, total }) {
   const navigate = useNavigate();
   const [grade, setGrade] = useState('keepPracticing');
   const [quizTypeUrl, setQuizTypeUrl] = useState('/');
-  const audioRef = useRef(null);
 
   useEffect(() => {
     const percentage = (result / total) * 100;
 
-    if (percentage === 100) {
-      setGrade('perfect');
-      audioRef.current.play();
-    } else if (percentage >= 90) setGrade('excellent');
+    if (percentage === 100) setGrade('perfect');
+    else if (percentage >= 90) setGrade('excellent');
     else if (percentage >= 80) setGrade('greatJob');
     else if (percentage > 50) setGrade('goodEffort');
 
@@ -45,8 +41,6 @@ export default function QuizResult({ result, total }) {
       {grade !== t('result.keepPracticing') && (
         <Confetti percentage={(result / total) * 100} />
       )}
-
-      <audio ref={audioRef} src={perfectSoundFile} />
     </Container>
   );
 }
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,17 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { QuestionBox, QuizHeader, QuizResult } from '../components';
 import { getQuizQuestions, quizTypes } from '../utils/quizTypes';
+import correctAudioFile from '../assets/sound/correct.mp3';
+import wrongAudioFile from '../assets/sound/wrong.mp3';
+import perfectAudioFile from '../assets/sound/yay.mp3';
+import clapsAudioFile from '../assets/sound/claps.mp3';
 
 export default function Quiz() {
   // eslint-disable-next-line
   const [t, i18n] = useTranslation('global');
+
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [WrongAnswersIndexes, setWrongAnswersIndexes] = useState([]);
   const [questions, setQuestions] = useState([]);
   const [questionHeading, setQuestionHeading] = useState('');
   const [isCompleted, setIsCompleted] = useState(false);
+
+  const correctAudioRef = useRef(null);
+  const wrongAudioRef = useRef(null);
+  const perfectAudioRef = useRef(null);
+  const clapsAudioRef = useRef(null);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -39,14 +49,30 @@ export default function Quiz() {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
       setIsCompleted(true);
+      clapsAudioRef.current.play();
     }
   };
 
   const submitAnswer = (answer) => {
     if (answer !== questions[currentQuestionIndex]['correctAnswer']) {
       setWrongAnswersIndexes([...WrongAnswersIndexes, currentQuestionIndex]);
+      wrongAudioRef.current.pause();
+      wrongAudioRef.current.currentTime = 0;
+      wrongAudioRef.current.play();
     } else {
-      setCorrectAnswers(correctAnswers + 1);
+      setCorrectAnswers((prevCorrectAnswers) => {
+        const correctAnswers = prevCorrectAnswers + 1;
+
+        if (correctAnswers === questions.length) {
+          perfectAudioRef.current.play();
+        } else {
+          correctAudioRef.current.pause();
+          correctAudioRef.current.currentTime = 0;
+          correctAudioRef.current.play();
+        }
+
+        return correctAnswers;
+      });
     }
   };
 
@@ -73,6 +99,11 @@ export default function Quiz() {
       ) : (
         <QuizResult total={questions.length} result={correctAnswers} />
       )}
+
+      <audio ref={correctAudioRef} src={correctAudioFile} />
+      <audio ref={wrongAudioRef} src={wrongAudioFile} />
+      <audio ref={perfectAudioRef} src={perfectAudioFile} />
+      <audio ref={clapsAudioRef} src={clapsAudioFile} />
     </>
   );
 }
